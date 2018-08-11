bb1cbb210d59fc24a4cf66ddeddcb38ae30b535a
adamdubicki <adam@freshworks.io>
2018-08-11 12:45:33 -0700

    completed part 1 and two of gatsby tutorial

---
+++ tutorial-part-two/src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default () => (
+  <div style={{ margin: '3rem auto', maxWidth: 600 }}>
+    <h1>Richard Hamming on Luck</h1>
+    <div>
+      <p>
+        From Richard Hamming's classic and must-read talk
+      </p>
+      <blockquote>
+        <p>
+          There is indeed an element of luck, and no there isn't.
+          <em>
+            The particular thing you do is luck, but that you do something is now
+          </em>
+        </p>
+      </blockquote>
+    </div>
+  </div>
+);
