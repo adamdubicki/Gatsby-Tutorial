d16133e007c5a7a0da03d079a29b41ff121c6c86
adamdubicki <adam@freshworks.io>
2018-08-12 14:41:19 -0700

    started tutorial part 4

---
+++ tutorial-part-four/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>About Pandas Eating Lots</h1>
+    <div>
+      <p>
+        We're the only site running about Pandas that eat a bunch.
+      </p>
+    </div>
+  </div>
+);
---
+++ tutorial-part-four/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      />
+    </div>
+  </div>
+);
